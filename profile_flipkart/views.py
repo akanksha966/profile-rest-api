dc74cbb8978d32e8ee52309d7ccd5b6b91b5548a
Akanksha <wahaneakansha2@gmail.com>
2022-12-01 09:39:05 +0530

    Creating serializers for the POST request

--- profile_flipkart/views.py
+++ profile_flipkart/views.py
@@ -1,11 +1,12 @@
 from rest_framework.views import APIView
 
 from rest_framework.response import Response
+from rest_framework import status
+from profile_flipkart import serializers
 
 
 class HelloApiVIew(APIView):
-
-    def get(self, request, format=None):
+   def get(self, request, format=None):
 
         an_api = [
 
@@ -21,3 +22,22 @@ class HelloApiVIew(APIView):
 
 
         return Response({"message":"hello world", "an_api" : an_api})
+   def post(self, request):
+
+        serializer = self.serializer_class(data=request.data)
+
+        print("Log for serializer", type(serializer))
+
+        if serializer.is_valid():
+
+            name=serializer.validated_data.get("name")
+
+            age=serializer.validated_data.get("age")
+
+            message = f"hello {name, age}"
+
+            return Response({'message' : message})
+
+        else:
+
+            return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
